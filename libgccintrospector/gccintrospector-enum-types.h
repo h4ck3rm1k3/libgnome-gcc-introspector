cec04556419fe3b68119927e82f253f847d07dce
James Michael DuPont <H4ck3rm1k3@flossk.org>
2012-03-11 18:51:21 +0000

    adding generated code for indexing, gir is being generated finally

---
+++ libgccintrospector/gccintrospector-enum-types.h
@@ -0,0 +1,19 @@
+
+/* Generated data (by glib-mkenums) */
+
+#ifndef __INTROSPECTOR_ENUM_TYPES_H__
+#define __INTROSPECTOR_ENUM_TYPES_H__
+
+#include <glib-object.h>
+
+G_BEGIN_DECLS
+
+/* enumerations from "gccintrospector.h" */
+GType g_cc_tree_code_enum_get_type (void) G_GNUC_CONST;
+#define INTROSPECTOR_TYPE_CC_TREE_CODE_ENUM (g_cc_tree_code_enum_get_type ())
+G_END_DECLS
+
+#endif /* __INTROSPECTOR_ENUM_TYPES_H__ */
+
+/* Generated data ends here */
+
